502561c2e4c667f497ec8df73cacf090ca556a88
Roman <Nuadire@gmail.com>
2020-07-16 17:33:37 +0500

    fix

--- src/_constants/routs.constants.js
+++ src/_constants/routs.constants.js
@@ -1,9 +1,8 @@
 export const URL =  "https://conduit.productionready.io/api/";
-
 export const ROUTS = {
   "routHome": "/",
   "routLogin": "/login",
   "routSignUp": "/signup",
   "login": `${URL}users/login`,
   "register": `${URL}users`
-}
+};
