17df1535e7775e17dfd11968c99dfe495ec5c5e1
Roman <Nuadire@gmail.com>
2020-07-30 14:41:39 +0500

    fix

--- src/components/Like/Like.jsx
+++ src/components/Like/Like.jsx
@@ -4,28 +4,36 @@ import PropTypes from "prop-types";
 import { favoritArticle, unfavoritArticle } from "../../_actions";
 import "./Like.scss";
 
-
 class Like extends React.PureComponent {
-
   toggleFavorites = () => {
-    const {favorited, slug, unfavorit, favorit, userToken} = this.props;
+    const { favorited, slug, unfavorit, favorit, userToken } = this.props;
     if (favorited) {
       unfavorit(slug, userToken);
     } else {
       favorit(slug, userToken);
     }
-  }
+  };
 
-  render () {
-    const {favorited, favoritesCount, isPublic} = this.props;
+  render() {
+    const { favorited, favoritesCount, isPublic } = this.props;
     return (
       <label className="like">
-        {
-          isPublic ? 
-          <input type="checkbox" className="like__checkbox-hide" checked={false} readOnly /> :
-          <input type="checkbox" className="like__checkbox-hide" checked={favorited} onChange={this.toggleFavorites} /> 
-        }
-        <span className="like__icon"  />
+        {isPublic ? (
+          <input
+            type="checkbox"
+            className="like__checkbox-hide"
+            checked={false}
+            readOnly
+          />
+        ) : (
+          <input
+            type="checkbox"
+            className="like__checkbox-hide"
+            checked={favorited}
+            onChange={this.toggleFavorites}
+          />
+        )}
+        <span className="like__icon" />
         {favoritesCount}
       </label>
     );
@@ -39,26 +47,31 @@ Like.propTypes = {
   isPublic: PropTypes.bool.isRequired,
   favoritesCount: PropTypes.number,
   slug: PropTypes.string.isRequired,
-  userToken: PropTypes.string.isRequired
-}
+  userToken: PropTypes.string,
+};
 
 Like.defaultProps = {
-  // favorited: false,
-  favoritesCount: 0
-}
+  userToken: "",
+  favoritesCount: 0,
+};
 
 function mapState(state) {
   const { user } = state.authentication;
+  if (user) {
+    return {
+      isPublic: false,
+      userToken: user.token,
+    };
+  }
   return {
-    isPublic: !user,
-    userToken: user.token
+    isPublic: false,
   };
 }
 
 const actionCreator = {
   favorit: favoritArticle,
-  unfavorit: unfavoritArticle
-}
+  unfavorit: unfavoritArticle,
+};
 
 const connectedLike = connect(mapState, actionCreator)(Like);
 export { connectedLike as Like };
